7e18a1c0f3eada6cc1a3412dbe93be1b46d0d0e8
James0251 <den4643@yandex.ru>
2020-06-15 18:32:39 +0300

    Создана валидация на общий объем загружаемых файлов

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UploadController extends Controller
 {
@@ -12,31 +13,17 @@ class UploadController extends Controller
     }
 
     public function upload(Request $request){
-//        dd($request->file('images'));
-//        $rules = [];
-        $max_size = 1000000;
-//        foreach($request->file('images') as $key => $val){
-//            $rules['image.'.$key] = "['required|mimes:bmp,png,jpg']|max:{$max_size}";
-////            $rules['image'] = 'max:'.$max_size;
-//        }
-//        dd($rules);
 
-
-
+        $max_size = 1024;
 
         $request->validate([
-            'images' => 'required',
-            'images.*' => 'mimes:bmp,png,jpg',
+            'images' => 'array|required|max_uploaded_file_size:'.$max_size,
+            'images.*' => 'mimetypes:image/jpeg,image/png,image/bmp',
             ]);
-        foreach ($request->file() as $file){
-            foreach ($file as $f){
-//                dd($f);
-                $size[] = $f->getSize();
-                $sum = array_sum($size);
-                if ($sum <= $max_size){
-                    $f->move(storage_path('images'), time().'_'.$f->getClientOriginalName());
-                }else{echo "Error";}
 
+        foreach ($request->file() as $files){
+            foreach ($files as $file){
+                $file->move(storage_path('images'), time().'_'.$file->getClientOriginalName());
             }
         }
         return "Успех";
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Validator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('max_uploaded_file_size', function ($attribute, $value, $parameters, $validator) {
+
+            $total_size = array_reduce($value, function ( $sum, $item ) {
+                // each item is UploadedFile Object
+                $sum += filesize($item->path());
+                return $sum;
+            });
+
+            // $parameters[0] in kilobytes
+            return $total_size < $parameters[0] * 1024;
+
+        });
+
     }
 }
